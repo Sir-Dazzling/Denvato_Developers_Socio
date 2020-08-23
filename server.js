dbf85a7ae125f1eb1d124263130c427d7de655c8
Sir-Dazzling <ottidan20@gmail.com>
2020-08-23 15:25:50 +0100

    set up basic express api

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+//Initialize app variable with express
+const app = express();
+
+app.get("/", (req, res) => res.send("API is running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
