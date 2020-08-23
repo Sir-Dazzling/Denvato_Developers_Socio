08a777598766d3fad4b0ca5967875a0998a98c88
Sir-Dazzling <ottidan20@gmail.com>
2020-08-23 15:47:02 +0100

    configured server and db conncection

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+//Connect to db
+const connectDB = async() => 
+{
+    try
+    {
+      await mongoose.connect(db, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      });
+
+      console.log("MongoDB connected");
+    }catch(error)
+    {
+        console.error(error.message);
+
+        //Exit process with failure status
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 //Initialize app variable with express
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("API is running"));
 
