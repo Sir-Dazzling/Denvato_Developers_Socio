56c91d7156101ee689d50e31c34b5b9759dd0761
Sir-Dazzling <ottidan20@gmail.com>
2020-08-24 18:54:16 +0100

    started working on posts model and routes

--- models/User.js
+++ models/User.js
@@ -29,4 +29,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = (User) = mongoose.model("user", UserSchema);
+module.exports = (User) = mongoose.model("users", UserSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const {check, validationResult}  = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
-// @route GET api/posts
+// @route POST api/posts
 // @desc Test route
-// @access Public
-router.get("/", (req, res) => res.send("Posts Route"));
+// @access Private
+router.post("/", [auth, [
+    check("text", "Text is required").not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    try 
+    {
+        const user = await User.findById(req.user.id).select("-password");
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });   
+
+        const post = await newPost.save();
+        res.json(post);
+
+    } catch (error) 
+    {
+        console.error(error.message);
+        res.status(500).send("Server Error");   
+    }
+});
 
 module.exports = router;
